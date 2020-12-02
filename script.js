a5520751b823cd5cbe28e20c6c26eec21be7cbe4
hvzy <hadimirza1@gmail.com>
2020-12-02 15:09:19 -0500

    initial code

---
+++ script.js
@@ -0,0 +1,17 @@
+/*----- app's state (variables) -----*/
+
+let translation = ""
+
+/*----- cached element references -----*/
+
+let input1 = document.getElementById("input1")
+let span1 = document.getElementById("span1")
+let btn1 =  document.getElementById("btn1")
+
+/*----- event listeners -----*/
+
+btn1.addEventListener("click", translate) {
+    alert("clicked")
+}
+
+/*----- functions -----*/
